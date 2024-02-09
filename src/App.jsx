e424d772a93398722d5a58aca3b3a35efd5694e4
jmathison21 <jennamath21@gmail.com>
2024-02-09 17:02:44 -0500

    logo and folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 //import { createClient } from "@supabase/supabase-js"
+import Header from "./components/Header/Header.jsx"
 
 //const supabase = createClient("https://mzxggfzyoilykqisvenx.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im16eGdnZnp5b2lseWtxaXN2ZW54Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc1MDk1MzAsImV4cCI6MjAyMzA4NTUzMH0.cbDp546DDaAUD7hx0ByXFp-ifweprGmxU-m8lTGTqPA")
 
@@ -6,7 +7,7 @@ function App() {
 
   return (
     <div id="App">
-      <h1 id="title">VendUMD</h1>
+      <Header />
     </div>
   )
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import "./Header.css"
+
+export default function Header() {
+    return (
+        <div id="header">
+            <h1 id="logo">VendUMD</h1>
+        </div>
+    )
+}
