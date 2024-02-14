cfa2c1d79c226ff385e025f230d33cc1c809c45f
jmathison21 <jennamath21@gmail.com>
2024-02-14 13:23:50 -0500

    express

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 //import { createClient } from "@supabase/supabase-js"
+import { useEffect, useState } from "react"
 import Header from "./components/Header/Header.jsx"
+import "./App.css"
 
 //const supabase = createClient("https://mzxggfzyoilykqisvenx.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im16eGdnZnp5b2lseWtxaXN2ZW54Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc1MDk1MzAsImV4cCI6MjAyMzA4NTUzMH0.cbDp546DDaAUD7hx0ByXFp-ifweprGmxU-m8lTGTqPA")
 
+
 function App() {
+  const [machines, setMachines] = useState(null)
+
+  useEffect(() => {
+    fetch("/api/machines")
+    .then((res) => {
+      if(!res.ok) return null
+      else return res.text()
+    })
+    ?.then((text) => setMachines(text))
+
+  })
 
   return (
     <div id="App">
       <Header />
+      <h2>{machines}</h2>
     </div>
   )
 }
