2f0c4fe35e571ba1e64c1912edb3a519925416d4
Quan Doan <ducqdoan@umich.edu>
2024-02-28 22:58:59 -0500

    Follow Skeleton

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,5 @@
 import "./Sidebar.css"
-
+import MachineView from "./MachineView/MachineView"
 function SidebarDiv({isDesktop, children}) {
     return (
         <>
@@ -16,6 +16,7 @@ export default function Sidebar({isDesktop}) {
     return (
         <SidebarDiv isDesktop={isDesktop}>
             <h3>Sidebar Section</h3>
+            <MachineView/>
         </SidebarDiv>
     )
 }
